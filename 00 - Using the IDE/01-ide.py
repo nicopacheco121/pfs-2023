ff13ad99e179593f2a65ddffeb6e51ce4ce0d642
nicolaspacheco <nicopacheco@gmail.com>
2023-08-06 12:50:56 -0300

    Clase 9

--- 00 - Using the IDE/01-ide.py
+++ 00 - Using the IDE/01-ide.py
@@ -2,13 +2,34 @@
 PyCharm documentation
 
 https://www.jetbrains.com/help/pycharm/2021.2/quick-start-guide.html
-
-
 '''
+
+"""
+Pestaña izquierda vemos el proyecto.
+Haciendo doble click entramos en el archivo, pasando a la segunda pantalla, que sería la pantalla actual.
+Pestañas en la parte inferior:
+- Python Packages
+- Archivo para commit, pasa de blanco a azul (Pycharm graba constantemente, no hace falta guardar)
+Local history para ver cambios realizados
+Atajos copiar pegar deshacer
+Barra derecha y vemos el error
+Run y vemos el error a fondo
+
+Debug
+- Breakpoings
+- Set value
+- F8 y F9
+- Consola
+- Evaluate expression
+- Breakpoing condicional
+
+
+"""
+
 
 #error highlighting
 variable4 = "abc"
-operand2 = 2
+operand2 = '2'
 #Refactor
 print(variable4 + operand2)
 
