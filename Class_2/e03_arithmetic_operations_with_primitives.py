5a0dba1190c493307de7f9d1b12db5e27ffce1a8
nicolaspacheco <nicopacheco@gmail.com>
2023-08-18 09:33:11 -0300

    leves cambios y comentarios para la clase

--- Class_2/e03_arithmetic_operations_with_primitives.py
+++ Class_2/e03_arithmetic_operations_with_primitives.py
@@ -9,55 +9,54 @@ Operator	Name	        Example	Try it
 //	        Floor division	x // y
 '''
 
-
+# Operaciones aritmeticas que puedo hacer entre strings
 operand1 = "string1"
 operand2 = "string2"
-variable = operand1
 
-print(operand1+operand2)
-print(operand1-operand2)
-print(operand1*operand2)
-print(operand1/operand2)
-print(operand1%operand2)
-print(operand1**operand2)
 
-operand1 = 4
+# print(operand1+operand2)
+# print(operand1-operand2)
+# print(operand1*operand2)
+# print(operand1/operand2)
+# print(operand1%operand2)
+# print(operand1**operand2)
+
+# Operaciones aritmeticas que puedo hacer entre enteros
+operand1 = 11
 operand2 = 3
 
-print(operand1+operand2)
-print(operand1-operand2)
-print(operand1*operand2)
-print(operand1/operand2)
-print(operand1%operand2)
-print(operand1**operand2)
+print(operand1 + operand2)
+print(operand1 - operand2)
+print(operand1 * operand2)
+print(operand1 ** operand2)
+print(operand1 / operand2)
+print(operand1 % operand2)  # por ejemplo se puede utilizar para encontrar multiplos
+print(operand1 // operand2)  # por ejemplo se puede utilizar para obtener los nominales a operar
 
+# Operaciones aritmeticas que puedo hacer entre un string y un numero
 operand1 = "-"
 operand2 = 10
 
-print(operand1+operand2)
-print(operand1-operand2)
+# print(operand1+operand2)
+# print(operand1-operand2)
 print(operand1*operand2)
-print(operand1/operand2)
-print(operand1%operand2)
-print(operand1**operand2)
+# print(operand1/operand2)
+# print(operand1%operand2)
+# print(operand1**operand2)
 
-operand1 = 2
-operand2 = 2
-
-print(operand1+operand2)
-print(operand1-operand2)
-print(operand1*operand2)
-print(operand1/operand2)
-print(operand1%operand2)
-print(operand1**operand2)
+# Quiero modificar el contenido de mi variable aplicandole algun operador algebraico
+operand1 = 0
+operand1 += 1
+operand1 = operand1 + 1
 operand1 += 1
 print(operand1)
 #esto mismo se puede hacer con todos los operadores
-operand1 *=4
+operand1 *= 4
 print(operand1)
 
-operand1 = False #0
-operand2 = True #1
+# Operaciones aritmeticas entre booleanos. Se comportan como si fueran ceros o unos.
+operand1 = False  # 0
+operand2 = True  # 1
 
 print(operand1+operand2)
 print(operand1-operand2)
@@ -78,9 +77,5 @@ print(True+True)
 /=	        x /= 3	x = x / 3	
 %=	        x %= 3	x = x % 3	
 //=	        x //= 3	x = x // 3	
-**=	        x **= 3	x = x ** 3	
-&=	        x &= 3	x = x & 3	
-|=	        x |= 3	x = x | 3	
-^=	        x ^= 3	x = x ^ 3	
->>=	        x >>= 3	x = x >> 3	
-<<=	        x <<= 3	x = x << 3'''
+**=	        x **= 3	x = x ** 3
+'''
