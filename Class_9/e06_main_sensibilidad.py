cdc42b5dd816f8495c728512532b8102d6929d58
nicolaspacheco <nicopacheco@gmail.com>
2023-10-16 13:31:26 -0300

    comentarios para la clase

--- Class_9/e06_main_sensibilidad.py
+++ Class_9/e06_main_sensibilidad.py
@@ -16,7 +16,7 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True,
 
     start_time = time.time()
 
-    # Resultados acumulados
+    # Resultados acumulados donde vamos a guardar los resultados de cada combinacion de indicadores
     resultados = []
 
     # - - - DESCARGA DE DATOS - - -
@@ -37,23 +37,29 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True,
     # print(emas_fast, emas_slow)
 
     # - - - CALCULO DE INDICADORES - - -
-    # data['rsi'] = indicadores.add_rsi(data, 'c', 14)
+    # Ahora los indicadores los calculamos dentro del for
     # Borro columnas que no voy a usar
     data.drop(['o', 'h', 'l', 'v', 'v_q', 'n'], axis=1, inplace=True)
 
+    print(f'Calculando valores de rsi: {config.SENS_RSI} y emas: {emas_slow} y {emas_fast}')
+
     for rsi in config.SENS_RSI:
+        # por cada valor de rsi, calculo el rsi y las emas
         data['rsi'] = indicadores.add_rsi(data, 'c', rsi)
 
         for ema_slow in emas_slow:
-            for ema_fast in emas_fast:
 
+            data['ema_slow'] = indicadores.add_ema(data, 'c', ema_slow)
+
+            for ema_fast in emas_fast:
                 if ema_fast >= ema_slow:  # Si la ema_fast es mayor o igual a la ema_slow, no la calculo
                     continue
+
+                # cuantas veces corre el codigo?
 
                 # print(f'Calculando con ema slow: {ema_slow}, ema fast: {ema_fast}')
                 print(f'Calculando con rsi: {rsi}, ema fast: {ema_fast}, ema slow: {ema_slow}')
 
-                data['ema_slow'] = indicadores.add_ema(data, 'c', ema_slow)
                 data['ema_fast'] = indicadores.add_ema(data, 'c', ema_fast)
 
                 data.dropna(inplace=True)
@@ -80,7 +86,7 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True,
     # Ordeno los resultados por profit
     resultados.sort_values(by='resultadodo_acumulado', ascending=False, inplace=True)
 
-    print(resultados.head(20))
+    # print(resultados.head(20))
 
     print(f'Tiempo de ejecución: {time.time() - start_time} segundos')
     print(f'Cantidad de resultados: {len(resultados)}')
@@ -89,7 +95,7 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True,
 
 
 if __name__ == '__main__':
-    run('BTCUSDT', '1h', False)
-    # data = pd.read_pickle(f'resultados_BTCUSDT')
-    # print(data.head(20))
+    # run('BTCUSDT', '1h', False)
+    data = pd.read_pickle(f'resultados_BTCUSDT')
+    print(data.head(20))
 
