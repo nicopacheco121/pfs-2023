b466751dbe7f06130eecb9dc0fd89d4a84cac138
nicolaspacheco <nicopacheco@gmail.com>
2023-10-16 10:29:28 -0300

    comentarios para la clase

--- Class_9/e00_main.py
+++ Class_9/e00_main.py
@@ -4,6 +4,7 @@ from e03_operaciones import signals, operations
 from e04_trades import make_trades, make_results
 import e05_plots as plots
 
+
 import config
 import pandas as pd
 
@@ -13,10 +14,10 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True):
     # - - - DESCARGA DE DATOS - - -
     # (Ver solo el get_data_binance)
     if descargar_data:
-        data, bad_request = download_data(symbol, timeframe, '2020-01-01', '2023-06-03', api='PERPETUOS')
+        data, bad_request = download_data(symbol, timeframe, '2020-01-01', '2023-10-20', api='PERPETUOS')
         print('bad_request: ', bad_request)
 
-        # Procesamos los datos a float y datetime
+        # Procesamos los datos a float y datetime ya que llega en formato string
         data = data.astype(float)
         data.index = pd.to_datetime(data.index, unit='ms')
         print(data)
@@ -31,13 +32,14 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True):
     data['ema_slow'] = indicadores.add_ema(data, 'c', config.EMA_SLOW)
     data['ema_fast'] = indicadores.add_ema(data, 'c', config.EMA_FAST)
     data['rsi'] = indicadores.add_rsi(data, 'c', 14)
-    # Borro columnas que no voy a usar
-    data.drop(['o', 'h', 'l', 'v', 'v_q', 'n'], axis=1, inplace=True)
-    data.dropna(inplace=True)
 
-    # Plot de indicadores
-    # (ver la funcion)
+    data.drop(['o', 'h', 'l', 'v', 'v_q', 'n'], axis=1, inplace=True)  # Borro columnas que no voy a usar
+    data.dropna(inplace=True)   # Borro los NaN que se generan al calcular los indicadores
+
+    # # Plot de indicadores
+    # # (ver la funcion)
     # plots.plot_indicators(data.iloc[-1000:], 'c', 'ema_slow', 'ema_fast', 'rsi')
+    # # Le paso los ultimos 1000 datos para que no quede tan cargado el grafico
 
     # - - - SEÑALES - - -
     data['signal'] = signals(data, 'ema_slow', 'ema_fast', 'rsi')
@@ -49,17 +51,18 @@ def run(symbol, timeframe=config.TIMEFRAME, descargar_data=True):
     # print(operaciones)
 
     # - - - TRADES - - -
-    trades = make_trades(operaciones)
+    trades = make_trades(operaciones, side='long')
     # print(trades)
 
     # Plot de los trades
     # plots.plot_trades(data)
 
-    # - - - RESULTADOS - - -
+    # # - - - RESULTADOS - - -
     resultado = make_results(trades)
     print(resultado)
 
 
 if __name__ == '__main__':
-    run('ETHUSDT', descargar_data=True)
+    # con el __name__ == '__main__' le decimos que solo corra el codigo si estamos en este archivo
+    run('ETHUSDT', descargar_data=False)
 
